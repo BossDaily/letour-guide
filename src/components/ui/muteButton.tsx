485afce71a620127ccb564aa619891c4ce7e386d
Rutledge Dixon <rutledgedixon@letu.edu>
2025-10-30 14:05:28 -0500

    changed mute button to be its own file

---
+++ src/components/ui/muteButton.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function MuteButton() {
+  const [muted, setMuted] = useState(false);
+
+  const toggleMute = () => {
+    setMuted(!muted);
+    const audioElement = document.querySelector("audio");
+    if (audioElement) {
+      audioElement.muted = !muted;
+    }
+  };
+
+  return (
+    <Button 
+        onClick={toggleMute}
+        variant="letu"
+        className="mt-[1vw] text-[28px] font-bold w-full max-w-[500px] h-14 px-6 py-2"
+    >
+      {muted ? "Unmute" : "Mute"}
+    </Button>
+  );
+};
