0456049f22dd705682fb0ab1dbc8c31a04b4d382
BossDaily <gboys2008@gmail.com>
2025-11-18 11:57:29 -0600

    changed label from mute to deafen

--- src/components/ui/muteButton.tsx
+++ src/components/ui/muteButton.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export default function MuteButton() {
   const [muted, setMuted] = useState(true);
+  
 
   const toggleMute = () => {
     const newMutedState = !muted;
@@ -20,7 +21,8 @@ export default function MuteButton() {
         variant="letu"
         className="mt-[1vw] text-[28px] font-bold w-full max-w-[500px] h-14 px-6 py-2"
     >
-      {muted ? "Unmute" : "Mute"}
+        {muted ? "Undeafen" : "Deafen"}
+      
     </Button>
   );
 }
